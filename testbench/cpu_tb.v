fd6bbdd23b5866c66af2278e69146bd21c201a3b
DavidVentura <davidventura27@gmail.com>
2024-01-03 15:31:41 +0100

    move memory module to file

--- testbench/cpu_tb.v
+++ testbench/cpu_tb.v
@@ -1,3 +1,4 @@
+`define DEBUG 1
 module rom(
 	input clk,
 	input [31:0] addr,
@@ -37,46 +38,10 @@ module rom(
 		end
 	end
 endmodule
-module memory(
-	input clk,
-	input [31:0] addr,
-	input [7:0] data_in,
-	output [7:0] data_out,
-	input memory_read_en,
-	input memory_write_en,
-	output ready
-);
-	// TODO: this caps out at 255
-	reg [7:0] mem [0:255];
-	reg [7:0] data_out_r;
-
-	reg [31:0] last_addr = 0;
-
-	reg ready_r = 0;
-	assign ready = ready_r;
-	assign data_out = data_out_r;
-
-	always @(posedge clk) begin
-		if (memory_read_en && addr !== last_addr) begin
-			$display("[MEM] Read  %x from %x", mem[addr & 8'hff], addr);
-			data_out_r <= mem[addr & 8'hff];
-			last_addr <= addr;
-			ready_r <= 1;
-		end else begin
-			ready_r <= 0;
-			if (memory_write_en) begin
-				last_addr <= 1'bx;
-				$display("[MEM] Wrote %x to   %x", data_in, addr);
-				mem[addr & 8'hff] <= data_in;
-			end
-		end
-	end
-endmodule
 
 module cpu_tb;
 	reg clk = 0;
 	always #1 clk <= ~clk;
-	wire [31:0] mem_addr2;
 	wire [31:0] mem_addr;
 	wire [7:0] mem_data_in;
 	wire [7:0] mem_data_out;
